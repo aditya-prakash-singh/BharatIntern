aec160abec7c48f0fc8abbf51e11b717aaf28fa3
aditya-prakash-singh <126975871+aditya-prakash-singh@users.noreply.github.com>
2023-08-13 00:05:33 +0530

    Temperature Converter Webapp Code

---
+++ temp/script.js
@@ -0,0 +1,36 @@
+var cel = document.getElementById("cel");
+var fah = document.getElementById("fah");
+var kel = document.getElementById("kel");
+// celcius to fahrenhite and kelvin
+
+cel.addEventListener("input", function () {
+    let c = this.value;
+    let f = (c * 9) / 5 + 32;
+    let k = c++ + 273.15;
+    fah.value = f;
+    kel.value = k;
+});
+
+// farhenhite to celcius and kelvin
+fah.addEventListener("input", function () {
+    let f = this.value;
+    let c = ((f - 32) * 5) / 9;
+    let k = ((f - 32) * 5) / 9 + 273.15;
+    cel.value = c;
+    kel.value = k;
+});
+
+//  kelvin to celcius and fahrenhinte
+kel.addEventListener("input", function () {
+    let k = this.value;
+    let c = k - 273.15;
+    let f = ((k - 273.15) * 9) / 5 + 32;
+    cel.value = c;
+    fah.value = f;
+});
+
+function reset() {
+    document.getElementById("cel").value = 0;
+    document.getElementById("fah").value = 32;
+    document.getElementById("kel").value = 273.15;
+}
